455bbfcb9d85478b622946d6d3768ebb0981ab72
Matthew Fritschi <mfrit1@brockport.edu>
2019-04-01 19:18:07 -0400

    Update GlobalVariables.java

--- KSSPE/utilities/GlobalVariables.java
+++ KSSPE/utilities/GlobalVariables.java
@@ -18,4 +18,8 @@ public class GlobalVariables
 	public static final int BARCODEPREFIX_LENGTH = 3;
 	public static final int CATEGORYNAME_LENGTH = 50;
 	public static final int PENALTY_LENGTH = 20;
+	public static final int BARCODE_LENGTH = 10;
+	public static final int EQUIPMENT_NAME_LENGTH = 50;
+	public static final int EQUIPMENT_COUNT_LENGTH = 1000;
+	public static final int NOTE_LENGTH = 50;
 }
